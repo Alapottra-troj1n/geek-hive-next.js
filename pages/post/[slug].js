8b73d2645f7bd528830408e92d6bd36f53a12f89
Alapottra <alapottratrojon@gmail.com>
2022-08-29 15:44:23 +0600

    dynamic route top part

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React from 'react';
 
@@ -8,9 +9,23 @@ const SinglePost = () => {
 
     return (
         <div className="h-[80vh]">
-            <div className="pt-20">
+            <div className="pt-28">
 
-                <h2>{slug}</h2>
+           <div className="flex justify-center">
+           <div className="h-[500px] w-[450px] relative">
+                <Image src={'/ala.jpg'} layout='fill' alt='' />
+                <div className="absolute bottom-0 p-2 bg-slate-900 px-8 font-type text-xl font-black text-white">
+                    GAMING
+                </div>
+            </div>
+           </div>
+
+           {/* text area */}
+
+           <div className="max-w-7xl  mx-auto py-10 font-type font-black text-5xl text-white" >
+                <h2>MLBB IS CURRENTLY DOMINATING MOBILE MARKET</h2>
+
+           </div>
 
 
             </div>
