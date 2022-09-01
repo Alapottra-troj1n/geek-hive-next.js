a6260f82a059fbaf5a4bde01a99f4cbbbcb10910
Alapottra <alapottratrojon@gmail.com>
2022-09-01 12:11:51 +0600

    added login signup swap functionality

--- components/LoginPage.jsx
+++ components/LoginPage.jsx
@@ -1,28 +1,66 @@
 import React, { useState } from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const LoginPage = () => {
 
-    const [page, setPage] = useState('login')
+    const [page, setPage] = useState('signin')
 
+    if (page === 'signup') {
 
+        return (
+            <>
+
+                <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-30" >
+
+                    <div className="p-16 bg-slate-700 border-2 border-main">
+                        <div className="absolute top-2 right-3" ><AiOutlineClose className="text-main text-xl cursor-pointer " /></div>
+                        <div>
+                            <h2 className='font-type font-black text-center pb-7 text-3xl' >SIGN UP</h2>
+                        </div>
+                        <form className="flex flex-col w-80 gap-4">
+
+                            <input type="email" placeholder="Email" name='signup-email' className="input w-full border-2 focus:border-main bg-white" />
+                            <input type="text" placeholder="Username" name='signup-username' className="input w-full border-2 focus:border-main bg-white" />
+                           <div>
+                           <input type="password"  placeholder="Password" name='signup-password' className="input w-full border-2 focus:border-main bg-white" />
+                           </div>
+                            <input type="password" placeholder="Confirm Password" name='signup-confirmpassword' className="input w-full border-2 focus:border-main bg-white" />
+                            <input type="submit" value="SIGN UP" className='px-2 cursor-pointer bg-main font-type font-black py-3 text-black rounded-lg' />
+
+                        </form>
+                        <div className="pt-2">
+                            <h2 className='font-display font-bold'>Already Have an Account ? <span onClick={() => setPage('signin')} className='text-white cursor-pointer hover:text-main transition' >Sign in</span> </h2>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </>
+
+        )
+    }
 
 
 
     return (
         <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-30" >
-            
-            <div className="p-16 bg-slate-700 rounded border-2 border-main">
-            <div className="absolute top-2 right-5" >X</div>
+
+            <div className="p-16 bg-slate-700 border-2 border-main">
+                <div className="absolute top-2 right-3" ><AiOutlineClose className="text-main text-xl cursor-pointer " /></div>
                 <div>
-                    <h2 className='font-type font-black text-center pb-7 text-3xl' >LOGIN</h2>
+                    <h2 className='font-type font-black text-center pb-7 text-3xl' >SIGN IN</h2>
                 </div>
                 <form className="flex flex-col w-80 gap-4">
 
-                    <input type="email" placeholder="Email" className="input w-full border-2 focus:border-main bg-white" />
-                    <input type="password" placeholder="Password" className="input w-full border-2 focus:border-main bg-white" />
-
+                    <input type="email" placeholder="Email" name='login-email' className="input w-full border-2 focus:border-main bg-white" />
+                    <input type="password" placeholder="Password" name='login-password' className="input w-full border-2 focus:border-main bg-white" />
+                    <input type="submit" value="LOGIN" className='px-2 bg-main font-type font-black py-3 text-black rounded-lg cursor-pointer' />
 
                 </form>
+                <div className="pt-2">
+                    <h2 className='font-display font-bold'>New to Geek Hive ? <span onClick={() => setPage('signup')} className='text-white cursor-pointer hover:text-main transition' >Create a Account</span> </h2>
+                </div>
 
             </div>
 
