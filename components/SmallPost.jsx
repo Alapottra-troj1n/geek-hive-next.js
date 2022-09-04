4040c01446e292c6d52d371ea1e5cf5da88ea437
Alapottra <alapottratrojon@gmail.com>
2022-09-05 01:42:11 +0600

    timestamp added

--- components/SmallPost.jsx
+++ components/SmallPost.jsx
@@ -1,8 +1,13 @@
+import { data } from 'autoprefixer';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 const smallPost = ({postData}) => {
+
+   const timestamp = postData._id.toString().substring(0,8);
+   const date = new Date( parseInt( timestamp, 16 ) * 1000 );
+  
 
     return (
         <div className="bg-white mt-5 grid grid-cols-1 lg:grid-cols-2 items-center p-3 ">
@@ -10,7 +15,7 @@ const smallPost = ({postData}) => {
                 <Image src={postData.img} alt='' layout='fill' objectFit='cover'  />
             </div>
             <div>
-            <p className="font-display text-sm text-slate-500" >14 Dec / by {postData.author}</p>
+            <p className="font-display text-sm text-slate-500" >{date.toString().slice(3,15)} by {postData.author}</p>
             <Link href={'/post/this-is-a-post'}><h2 className="font-bold font-display max-w-lg leading-6 text-slate-700 text-lg hover:text-main cursor-pointer transition-all" >{postData.title.slice(0,30)}..</h2></Link>
             </div>
         </div>
