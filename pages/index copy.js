598343f1675a146f01354bff714988111c3060ee
Alapottra <alapottratrojon@gmail.com>
2022-10-12 19:36:57 +0600

    changed bg image

--- pages/index copy.js
+++ pages/index copy.js
@@ -14,10 +14,11 @@ export default function Home({ data }) {
   const programmingPosts = data.filter(post => post.category === 'programming');
   const miscPosts = data.filter(post => post.category === 'misc');
 
-  const [value, setValue] = useState(0);
-  const bg = ['https://i.ibb.co/qkDk3nj/1.jpg', 'https://i.ibb.co/bJghxwm/2.jpg', 'https://i.ibb.co/rtmssLk/3.jpg', 'https://i.ibb.co/nR3X2wJ/4.jpg']
+  const tag = ['1','2','3','4']
 
-  useEffect(() => {
+
+
+  useEffect(() =>{
 
     const shuffled = [...data].sort(() => 0.5 - Math.random());
 
@@ -25,12 +26,13 @@ export default function Home({ data }) {
 
 
 
+
+
   }
-    , [data])
+    ,[data])
 
 
-
-
+ 
 
   return (
     <div >
@@ -40,25 +42,23 @@ export default function Home({ data }) {
       </Head>
 
 
- 
-        <div className={`bg-[url('/2.jpg')] bg-black  bg-cover bg-no-repeat bg-center`}>
+      <div className="bg-[url('/1.jpg')]" >
 
-          <div className="pt-60 pb-40 px-5 lg:h-screen lg:p-0 grid grid-cols-1 lg:grid-cols-[60%,40%] items-center mx-auto max-w-7xl " >
-            <div className="flex flex-col justify-center items-start font-type">
-              <h2 className=" font-extrabold text-md lg:text-xl bg-white px-4 text-gray-800 whitespace-nowrap">GAMING AND CODING</h2>
-              <h2 className="font-black text-xl lg:text-4xl text-white mt-2 leading-7">THOUGHTS RELATED TO CODING AND GAMING</h2>
-              <p className='font-display text-sm lg:text-lg text-white mt-2'>Everything you ever wanted to know about Gaming, Tech and Programming.</p>
-            </div>
-
-            <div className="flex p-8 justify-center items-center">
-              {/* <Image src={'/ala.jpg'} width={1090} height={1440} alt='ala photo'/> */}
-            </div>
-
+        <div className="pt-60 pb-40 px-5 lg:h-screen lg:p-0 grid grid-cols-1 lg:grid-cols-[60%,40%] items-center mx-auto max-w-7xl " >
+          <div className="flex flex-col justify-center items-start font-type">
+            <h2 className=" font-extrabold text-md lg:text-xl bg-white px-4 text-gray-800 whitespace-nowrap">GAMING AND CODING</h2>
+            <h2 className="font-black text-xl lg:text-4xl text-white mt-2 leading-7">THOUGHTS RELATED TO CODING AND GAMING</h2>
+            <p className='font-display text-sm lg:text-lg text-white mt-2'>Everything you ever wanted to know about Gaming, Tech and Programming.</p>
           </div>
 
+          <div className="flex p-8 justify-center items-center">
+            {/* <Image src={'/ala.jpg'} width={1090} height={1440} alt='ala photo'/> */}
+          </div>
 
         </div>
-   
+
+
+      </div>
 
 
 
@@ -77,7 +77,7 @@ export default function Home({ data }) {
 
         <div className="pb-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-7xl mx-auto gap-10">
 
-          {shuffledData?.slice(0, 4).map(post => <BlogPostComponent post={post} key={post._id} />)}
+        {shuffledData?.slice(0,4).map(post => <BlogPostComponent post={post} key={post._id} />)}
 
 
 
@@ -92,19 +92,19 @@ export default function Home({ data }) {
             <span className="font-extrabold text-md bg-slate-800 px-6 text-main font-type w-full text-center md:w-auto  py-2">GAMING </span>
 
 
-            {gamingPosts?.slice(0, 3).map(post => <SmallPost postData={post} key={post._id} />)}
+           {gamingPosts?.slice(0,3).map(post => <SmallPost postData={post} key={post._id} />  )}
 
 
           </div>
 
           <div className='flex flex-col items-center mt-40  justify-center md:mt-0'>
             <span className=" font-extrabold text-md bg-slate-800 px-6 text-main font-type w-full text-center md:w-auto py-2">PROGRAMMING</span>
-            {programmingPosts?.slice(0, 3).map(post => <SmallPost postData={post} key={post._id} />)}
+            {programmingPosts?.slice(0,3).map(post => <SmallPost postData={post} key={post._id} />  )}
           </div>
 
           <div className='flex flex-col items-center mt-40  justify-center md:mt-0'>
             <span className=" font-extrabold text-md bg-slate-800 px-6 text-main font-type w-full text-center md:w-auto py-2">MISC</span>
-            {miscPosts?.slice(0, 3).map(post => <SmallPost postData={post} key={post._id} />)}
+            {miscPosts?.slice(0,3).map(post => <SmallPost postData={post} key={post._id} />  )}
           </div>
 
 
@@ -120,17 +120,17 @@ export default function Home({ data }) {
 
       <div className='px-5 py-10 lg:py-24 lg:px-0 max-w-7xl mx-auto flex flex-col-reverse lg:flex-row items-center gap-10'>
 
-        <div>
-          <h2 className='font-type font-black text-2xl lg:text-4xl text-white' >STAY CURIOUS.</h2>
-          <h2 className='font-display lg:text-xl mt-2' >We are still not sure how we got here, but we are excited about where we are going</h2>
-        </div>
+            <div>
+              <h2 className='font-type font-black text-2xl lg:text-4xl text-white' >STAY CURIOUS.</h2>
+              <h2 className='font-display lg:text-xl mt-2' >We are still not sure how we got here, but we are excited about where we are going</h2>
+            </div>
 
-        <div className='relative lg:h-[500px] lg:w-[800px] h-80 w-80' >
+            <div className='relative lg:h-[500px] lg:w-[800px] h-80 w-80' >
 
-          <Image src="/person3.png" objectFit='cover' layout='fill' alt="" />
+              <Image src="/person3.png" objectFit='cover' layout='fill'  alt="" />
 
 
-        </div>
+            </div>
 
 
       </div>
@@ -150,12 +150,12 @@ export async function getStaticProps(context) {
 
 
   const db = await connectDb();
-  const allPosts = await db.collection("blogposts").find({ pending: false }).toArray();
+  const allPosts = await db.collection("blogposts").find({pending: false}).toArray();
 
 
 
   return {
-    props: { data: JSON.parse(JSON.stringify(allPosts)) },
-    revalidate: 60, // will be passed to the page component as props
+    props: {data : JSON.parse(JSON.stringify(allPosts))},
+    revalidate:60, // will be passed to the page component as props
   }
 }
